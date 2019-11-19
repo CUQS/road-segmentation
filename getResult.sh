1e620d420fa3cb9d633ac478f76eeb492a08a844
CUQS <m18801251973@163.com>
2019-11-19 12:13:20 +0800

    result path change

--- getResult.sh
+++ getResult.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scp HwHiAiUser@192.168.1.2:/home/HwHiAiUser/HIAI_PROJECTS/ascend_workspace/segmentation/out/out_test.png /home/lab/segmentation_test/out_test.png
+scp HwHiAiUser@192.168.1.2:/home/HwHiAiUser/HIAI_PROJECTS/ascend_workspace/segmentation/out/out_test.png ./out_test.png
